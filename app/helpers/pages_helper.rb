2df30fb302a4233e2d024f4bbe3266150c9bd51d
Adrian Rangel <adrian.rangel@gmail.com>
2015-05-07 15:37:07 -0500

    show search parameters

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,2 +1,12 @@
 module PagesHelper
+  def print_query
+    # {"q"=>"busqueee", "state"=>"2", "jury"=>"40", "date"=>"05/07/2015", "controller"=>"pages", "action"=>"search"}
+    html = '<ul>'
+    html += "<li>Busqueda: #{params[:q]}</li>" unless params[:q].blank?
+    html += "<li>Estado: #{State.find(params[:state]).name}</li>" unless params[:state].blank?
+    html += "<li>Jurado: #{Tribunal.find(params[:jury]).name}</li>" unless params[:jury].blank?
+    html += "<li>Fecha: #{params[:date]}</li>" unless params[:date].blank?
+    html += '</ul>'
+    raw html
+  end
 end
