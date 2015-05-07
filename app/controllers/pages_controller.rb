dc563624be6b3dbd9eddecbcd7eaf6381e435be8
luissifu@xubuntu <luis.e.sifuentes@gmail.com>
2015-05-07 13:11:16 -0500

    added contact page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -33,6 +33,9 @@ class PagesController < ApplicationController
   def privacy
   end
 
+  def contact
+  end
+
   def status
     @states = State.all
     @incidents_by_date = Incident.where(created_at: Date.current..(Date.current - 3.months)).order(created_at: :desc).group_by { |incid| incid.created_at.day }.values
